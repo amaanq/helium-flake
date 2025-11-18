b2c2c41a4b98f8228eeba41cb75c328fd42d887a
Amaan Qureshi <git@amaanq.com>
2025-11-17 23:30:08 -0500

    all: init

---
+++ README.md
@@ -0,0 +1,96 @@
+# helium-flake
+
+A nix flake for [Helium](https://github.com/imputnet/helium-linux), a private, fast, and honest web browser
+
+## Usage
+
+### Run directly
+
+```bash
+nix run github:amaanq/helium2nix
+```
+
+### Install to profile
+
+```bash
+nix profile install github:amaanq/helium2nix
+```
+
+### Add to NixOS configuration
+
+```nix
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    helium = {
+      url = "github:amaanq/helium2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
+  };
+
+  outputs = inputs: {
+    nixosConfigurations.yourhost = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        {
+          environment.systemPackages = [
+            inputs.helium.packages.x86_64-linux.default
+          ];
+        }
+      ];
+    };
+  };
+}
+```
+
+### Build locally
+
+```bash
+git clone git@github.com:amaanq/helium-flake.git
+cd helium-flake
+nix build
+./result/bin/helium
+```
+
+## Supported Platforms
+
+- x86_64-linux
+- aarch64-linux
+
+## Updating
+
+To update to a new Helium release:
+
+1. Check the [latest release](https://github.com/imputnet/helium-linux/releases) and update the version in `flake.nix`:
+
+   ```nix
+   version = "0.6.4.1";  # Update this
+   ```
+
+2. Fetch the new hashes for both architectures:
+
+   ```bash
+   # x86_64
+   nix-prefetch-url https://github.com/imputnet/helium-linux/releases/download/0.6.4.1/helium-0.6.4.1-x86_64_linux.tar.xz
+   nix hash convert --hash-algo sha256 <hash-from-above>
+
+   # aarch64
+   nix-prefetch-url https://github.com/imputnet/helium-linux/releases/download/0.6.4.1/helium-0.6.4.1-arm64_linux.tar.xz
+   nix hash convert --hash-algo sha256 <hash-from-above>
+   ```
+
+3. Update the `sha256` values in `flake.nix` for both architectures
+
+4. Test the build for both architectures:
+
+   ```bash
+   nix build .#packages.x86_64-linux.helium
+   ./result/bin/helium --version
+
+   nix build .#packages.aarch64-linux.helium
+   ./result/bin/helium --version
+   ```
+
+## License
+
+GPL-3.0 (following upstream)
---
+++ flake.nix
@@ -0,0 +1,126 @@
+{
+  description = "Helium - A private, fast, and honest web browser";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      platforms = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs platforms;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          inherit (pkgs) lib;
+        in
+        {
+          helium =
+            let
+              pname = "helium";
+              version = "0.6.4.1";
+            in
+            pkgs.stdenv.mkDerivation {
+              inherit pname version;
+
+              src = pkgs.fetchurl {
+                url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-${
+                  if system == "aarch64-linux" then "arm64" else "x86_64"
+                }_linux.tar.xz";
+                sha256 =
+                  if system == "aarch64-linux" then
+                    "sha256-S5kF+K2Kwhqa0GG691NvnU/2frUCWL9BKrVK7y3bzSE="
+                  else
+                    "sha256-9xEVnGym579rR6RunS4HWYV3nLk1ODEIGfg8PtNDSUk=";
+              };
+
+              nativeBuildInputs = [
+                pkgs.makeWrapper
+                pkgs.autoPatchelfHook
+                pkgs.qt6.wrapQtAppsHook
+              ];
+
+              buildInputs = [
+                pkgs.glib
+                pkgs.gdk-pixbuf
+                pkgs.gtk3
+                pkgs.nspr
+                pkgs.nss
+                pkgs.dbus
+                pkgs.atk
+                pkgs.at-spi2-atk
+                pkgs.cups
+                pkgs.expat
+                pkgs.libxcb
+                pkgs.libxkbcommon
+                pkgs.at-spi2-core
+                pkgs.xorg.libX11
+                pkgs.xorg.libXcomposite
+                pkgs.xorg.libXdamage
+                pkgs.xorg.libXext
+                pkgs.xorg.libXfixes
+                pkgs.xorg.libXrandr
+                pkgs.mesa
+                pkgs.cairo
+                pkgs.pango
+                pkgs.systemd
+                pkgs.alsa-lib
+                pkgs.libdrm
+                pkgs.qt6.qtbase
+              ];
+
+              # Ignore Qt5 shim, qt5webengine is unmaintained & we're using Qt6
+              autoPatchelfIgnoreMissingDeps = [
+                "libQt5Core.so.5"
+                "libQt5Gui.so.5"
+                "libQt5Widgets.so.5"
+              ];
+
+              installPhase = ''
+                runHook preInstall
+
+                mkdir -p $out/bin $out/opt/helium
+                cp -r ./* $out/opt/helium/
+
+                makeWrapper $out/opt/helium/chrome-wrapper $out/bin/helium \
+                  --prefix LD_LIBRARY_PATH : "${
+                    lib.makeLibraryPath [
+                      pkgs.libGL
+                      pkgs.libva
+                    ]
+                  }"
+
+                mkdir -p $out/share/applications
+                cp $out/opt/helium/helium.desktop $out/share/applications/
+                substituteInPlace $out/share/applications/helium.desktop \
+                  --replace-fail 'chromium' 'helium'
+
+                mkdir -p $out/share/pixmaps
+                cp $out/opt/helium/product_logo_256.png $out/share/pixmaps/helium.png
+
+                runHook postInstall
+              '';
+
+              meta = {
+                inherit platforms;
+                description = "A lightweight music streaming service client";
+                homepage = "https://github.com/imputnet/helium-linux";
+                license = lib.licenses.gpl3Only;
+                mainProgram = "helium";
+              };
+            };
+
+          default = self.packages.${system}.helium;
+        }
+      );
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.default);
+    };
+}
